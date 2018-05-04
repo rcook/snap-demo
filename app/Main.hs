17c1f2d7120dcc7ba611321dfb674f0858b0ca9e
Richard Cook <rcook@rcook.org>
2018-05-04 16:22:05 -0700

    JWT local authentication

--- app/Main.hs
+++ app/Main.hs
@@ -1,38 +1,43 @@
-{-|
-Module      : Main
-Description : Main module
-Copyright   : (C) Richard Cook, 2018
-Licence     : MIT
-Maintainer  : rcook@rcook.org
-Stability   : stable
-Portability : portable
-
-This is the first sample from <http://snapframework.com/docs/tutorials/snap-api>
-with some @import@s and @LANGUAGE@ pragmas.
--}
-
 {-# LANGUAGE OverloadedStrings #-}
 
 module Main (main) where
 
-import Control.Applicative
-import Snap.Core
-import Snap.Http.Server
-import Snap.Util.FileServe
+import           CodeWorld.Auth
+                    ( AuthConfig
+                    , authMethod
+                    , authRoutes
+                    , getAuthConfig
+                    , optionallyAuthenticated
+                    )
+import           CodeWorld.Auth.Http (ok200Json)
+import           CodeWorld.Auth.Util (m)
+import qualified Data.ByteString.Char8 as Char8 (unpack)
+import           Snap.Core
+                    ( Method(..)
+                    , Snap
+                    , getParam
+                    , method
+                    , route
+                    )
+import           Snap.Http.Server (quickHttpServe)
+import           Snap.Util.FileServe (serveFile)
+import           System.Directory (getCurrentDirectory)
 
 main :: IO ()
-main = quickHttpServe site
+main = do
+    cwd <- getCurrentDirectory
+    authConfig <- getAuthConfig cwd
+    putStrLn $ "Authentication: " ++ authMethod authConfig
+    quickHttpServe (site authConfig)
 
-site :: Snap ()
-site =
-    ifTop (writeBS "hello world") <|>
-    route [ ("foo", writeBS "bar")
-          , ("echo/:echoparam", echoHandler)
-          ] <|>
-    dir "static" (serveDirectory ".")
+site :: AuthConfig -> Snap ()
+site authConfig = route $
+    [ ("app", method GET (serveFile "app.html"))
+    , ("command0", method POST (command0Handler authConfig))
+    ] ++ authRoutes authConfig
 
-echoHandler :: Snap ()
-echoHandler = do
-    param <- getParam "echoparam"
-    maybe (writeBS "must specify echo/param in URL")
-          writeBS param
+command0Handler :: AuthConfig -> Snap ()
+command0Handler = optionallyAuthenticated $ \_ -> do
+    Just x <- getParam "x"
+    Just y <- getParam "y"
+    ok200Json $ m [ ("result", Char8.unpack x ++ Char8.unpack y) ]
